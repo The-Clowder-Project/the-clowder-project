9fa564f718b4db53fe1cacd8785a235d57d5e815
Emily de Oliveira Santos <emily.de.oliveira.santos.tmf@gmail.com>
2025-04-26 15:55:35 -0300

    2025-04-26-15:52 Update

--- wget-clone.sh
+++ wget-clone.sh
@@ -2,3 +2,7 @@
 mkdir ../web-clone
 cd ../web-clone
 wget  -k -p -E -m -e robots=off http://127.0.0.1:5000/
+mkdir ../web-clone/127.0.0.1/static/tikzcd-images/dark-mode
+mkdir ../web-clone/127.0.0.1/static/webcompile-images/dark-mode
+cp tmp/tikz-cd/dark-mode/*.svg ../web-clone/127.0.0.1/static/tikzcd-images/dark-mode
+cp tmp/webcompile/dark-mode/*.svg ../web-clone/127.0.0.1/static/webcompile-images/dark-mode
