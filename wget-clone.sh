421256814dcd0a6240b1b2be7e288c1861190243
Emily de Oliveira Santos <emily.de.oliveira.santos.tmf@gmail.com>
2025-05-27 16:10:35 -0300

    2025-05-27-03:18 Update

--- wget-clone.sh
+++ wget-clone.sh
@@ -1,12 +1,13 @@
 #/bin/bash
-#mkdir ../web-clone
-#cd ../web-clone
-#wget  -k -p -E -m -e robots=off http://127.0.0.1:5000/
-#mkdir ../web-clone/127.0.0.1/static/tikzcd-images/dark-mode
-#mkdir ../web-clone/127.0.0.1/static/webcompile-images/dark-mode
-#mkdir ../web-clone/127.0.0.1/static/scalemath-images/
-#mkdir ../web-clone/127.0.0.1/static/scalemath-images/dark-mode
-cp tmp/tikz-cd/dark-mode/*.svg ../web-clone/127.0.0.1/static/tikzcd-images/dark-mode
-cp tmp/webcompile/dark-mode/*.svg ../web-clone/127.0.0.1/static/webcompile-images/dark-mode
-cp tmp/scalemath/*.svg ../web-clone/127.0.0.1/static/scalemath-images
-cp tmp/scalemath/dark-mode/*.svg ../web-clone/127.0.0.1/static/scalemath-images/dark-mode
+mkdir web-clone
+cd web-clone
+wget -k -p -E -m -e robots=off http://127.0.0.1:5000/
+ls web-clone/
+#mkdir -p web-clone/127.0.0.1/static/tikzcd-images/dark-mode
+#mkdir -p web-clone/127.0.0.1/static/webcompile-images/dark-mode
+#mkdir -p web-clone/127.0.0.1/static/scalemath-images/
+#mkdir -p web-clone/127.0.0.1/static/scalemath-images/dark-mode
+#cp tmp/tikz-cd/dark-mode/*.svg ../web-clone/127.0.0.1/static/tikzcd-images/dark-mode
+#cp tmp/webcompile/dark-mode/*.svg ../web-clone/127.0.0.1/static/webcompile-images/dark-mode
+#cp tmp/scalemath/*.svg ../web-clone/127.0.0.1/static/scalemath-images
+#cp tmp/scalemath/dark-mode/*.svg ../web-clone/127.0.0.1/static/scalemath-images/dark-mode
