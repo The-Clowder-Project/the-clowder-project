8d02cdb4602ed89ec1b3021dfa384cf4943461ae
Emily de Oliveira Santos <emily.de.oliveira.santos.tmf@gmail.com>
2025-06-12 17:10:09 -0300

    2025-06-12-16:43 Update

--- pictures/light-mode/monoidal-right-unity-of-id-otimes-sets/make.sh
+++ pictures/light-mode/monoidal-right-unity-of-id-otimes-sets/make.sh
@@ -3,6 +3,6 @@
 for n in {01..06}; do lualatex $n.tex; done
 for n in {01..06}; do pdf2svg $n.pdf $n.svg; done
 for n in {01..06}; do inkscape "$n.svg" --export-type=png --export-dpi=1200 --export-filename="$n.png"; done
-convert -delay 100 -loop 1 *.png animated.gif
+convert -delay 100 -loop 0 -dispose Background 01.png 02.png 03.png 04.png 05.png 06.png 06.png 06.png animated.gif
 cp animated.gif ../../../gerby-website/gerby/static/gifs/monoidal-right-unity-of-id-otimes-sets.gif
 cp *.svg ../../../gerby-website/gerby/static/slides/monoidal-right-unity-of-id-otimes-sets/
