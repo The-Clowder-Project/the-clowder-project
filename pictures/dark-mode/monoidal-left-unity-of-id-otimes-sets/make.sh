a826bb9ffd861d8f4080faea763a9c5164d5bc3a
Emily de Oliveira Santos <emily.de.oliveira.santos.tmf@gmail.com>
2025-06-22 01:42:30 -0300

    2025-06-22-01:42 Update

--- pictures/dark-mode/monoidal-left-unity-of-id-otimes-sets/make.sh
+++ pictures/dark-mode/monoidal-left-unity-of-id-otimes-sets/make.sh
@@ -4,8 +4,4 @@ for n in {01..06}; do lualatex $n.tex; done
 for n in {01..06}; do pdf2svg $n.pdf $n.svg; done
 for n in {01..06}; do inkscape "$n.svg" --export-type=png --export-dpi=1200 --export-filename="$n.png"; done
 convert -delay 100 -loop 0 -dispose Background 01.png 02.png 03.png 04.png 05.png 06.png 06.png 06.png animated.gif
-echo "TESTTESTTEST-BEGIN"
-ls ../../../gerby-website/gerby/static/gifs/
-ls ../../../gerby-website/gerby/static/gifs/dark-mode/
-echo "TESTTESTTEST-END"
 cp animated.gif ../../../gerby-website/gerby/static/gifs/dark-mode/monoidal-left-unity-of-id-otimes-sets.gif
--- pictures/dark-mode/monoidal-right-unity-of-id-otimes-sets-star/make.sh
+++ pictures/dark-mode/monoidal-right-unity-of-id-otimes-sets-star/make.sh
@@ -5,3 +5,7 @@ for n in {01..06}; do pdf2svg $n.pdf $n.svg; done
 for n in {01..06}; do inkscape "$n.svg" --export-type=png --export-dpi=1200 --export-filename="$n.png"; done
 convert -delay 100 -loop 0 -dispose Background 01.png 02.png 03.png 04.png 05.png 06.png 06.png 06.png animated.gif
 cp animated.gif ../../../gerby-website/gerby/static/gifs/dark-mode/monoidal-right-unity-of-id-otimes-sets-star.gif
+echo "TESTTESTTEST-BEGIN"
+ls ../../../gerby-website/gerby/static/gifs/
+ls ../../../gerby-website/gerby/static/gifs/dark-mode/
+echo "TESTTESTTEST-END"
